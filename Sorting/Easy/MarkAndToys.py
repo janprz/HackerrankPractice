bdd85f2b6f0a929a2a6a6cabe25b79e04c8dfb8e
Jan Przybyszewski <przybysz@student.agh.edu.pl>
2020-09-26 22:49:42 +0200

    Add exercises from 26th of September.

---
+++ Sorting/Easy/MarkAndToys.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices = sorted(prices)
+    n_toys = 0
+    i = 0
+    while True:
+        k = k-prices[i]
+        if k >=0:
+            n_toys += 1
+            i += 1
+        if k < 0:
+            break
+    return n_toys
+        
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
