023f271c392237f6eb4cba1e937ed01b8a371aaf
janprz <jasiek.przybyszewski@gmail.com>
2020-10-03 18:55:59 +0200

    Add exercises from 3rd October 2020.

---
+++ GreedyAlgorithms/Easy/MinimumAbsoluteDifferenceinanArray.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr = sorted(arr)
+    min_diff = math.inf
+    for i in range(1,len(arr)):
+        if abs(arr[i]-arr[i-1]) < min_diff:
+            min_diff = abs(arr[i]-arr[i-1])
+    return min_diff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
